85accfe9d78fd80238570fdad7dabc6d711fee65
MINYUKYUNG <minew0223@gmail.com>
2022-09-25 23:56:51 +0900

    Modify : 코드 스타일 변경

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -10,13 +10,13 @@ export const saveCart = atom<Record<K, V>>({
 interface payGuard {
   getParams: number
   num: number
-}
+};
 
 type K = string
 type V = {
   id: number,
   count: number
-}
+};
 
 export const updateCart = selector({
   key: 'updateCart',
@@ -24,14 +24,14 @@ export const updateCart = selector({
     return {};
   },
   set: ({ set }, payload) => { // 원본훼손O
-    const { getParams, num } = payload as payGuard
-    let save: Record<K, V> = {}
+    const { getParams, num } = payload as payGuard;
+    let save: Record<K, V> = {};
 
     if (localStorage.getItem('cart_data')) {
       save = JSON.parse(localStorage.getItem('cart_data') || '{}')
     } else {
       save = {}
-    }
+    };
 
     // saveCart 값 바꾸고 -> localStorage.setItem 업데이트
     if (save[getParams]) { // 있으면, 기존것에 추가
@@ -44,11 +44,10 @@ export const updateCart = selector({
       if (num === 1) {
         save[getParams] = { id: getParams, count: 1 };
       }
-    }
+    };
     
-    localStorage.setItem('cart_data', JSON.stringify(save))
-
-    set(saveCart, save)
+    set(saveCart, save);
+    localStorage.setItem('cart_data', JSON.stringify(save));
   }
 });
 
@@ -58,13 +57,13 @@ export const deleteItem = selector({
     return {};
   },
   set: ({ set, get }, payload) =>  { // 원본훼손O
-    const { id } = payload as { id: number }
-    let save: Record<K, V> = {}
+    const { id } = payload as { id: number };
+    let save: Record<K, V> = {};
     for (const key in get(saveCart)) {
       if (Number(key) !== id) save = {...save, [key]: get(saveCart)[key]};
-    }
+    };
 
-    set(saveCart, save)
-    localStorage.setItem('cart_data', JSON.stringify(save))
+    set(saveCart, save);
+    localStorage.setItem('cart_data', JSON.stringify(save));
   }
 });
