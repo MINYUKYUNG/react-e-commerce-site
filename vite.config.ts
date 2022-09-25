20edfe864be138370a48fb0f9039db7abc2cec15
MINYUKYUNG <minew0223@gmail.com>
2022-09-25 23:48:47 +0900

    Feature : 절대경로 적용 라이브러리 설치 및 파일 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@src', replacement: resolve(__dirname, 'src')},
+      { find: '@apis', replacement: resolve(__dirname, 'src/apis')},
+      { find: '@assets', replacement: resolve(__dirname, 'src/assets')},
+      { find: '@components', replacement: resolve(__dirname, 'src/components')},
+      { find: '@pages', replacement: resolve(__dirname, 'src/pages')},
+      { find: '@routes', replacement: resolve(__dirname, 'src/routes')},
+      { find: '@store', replacement: resolve(__dirname, 'src/store')},
+    ]
+  },
+});
